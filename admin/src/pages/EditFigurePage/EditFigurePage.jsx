dcd371b1859ba641caa664c9126ff0a4c1db96f9
PartySoonXd <vladbelom2005@gmail.com>
2024-08-01 13:32:38 +0700

    change redirect url

--- admin/src/pages/EditFigurePage/EditFigurePage.jsx
+++ admin/src/pages/EditFigurePage/EditFigurePage.jsx
@@ -4,7 +4,7 @@ import { useNavigate, useParams } from "react-router-dom";
 import { getFigureBySlug } from "../../http/figureAPI";
 import { FigureForm } from "../../components/FigureForm/FigureForm";
 import { update } from "../../http/figureAPI";
-import { ADMIN_ROUTE, NOT_FOUND_ROUTE } from "../../utils/consts"
+import { FIGURES_ROUTE, NOT_FOUND_ROUTE } from "../../utils/consts"
 import { observer } from "mobx-react-lite";
 
 const files = {}
@@ -32,7 +32,7 @@ export const EditFigurePage = observer(() => {
     const updateFigureHandler = async (data, galleryItems, historyItems ) => {
         try {
             await update(slug, data, galleryItems, historyItems).then(() => {
-                history(ADMIN_ROUTE)
+                history(FIGURES_ROUTE)
             })
         } catch (error) {
             return error
