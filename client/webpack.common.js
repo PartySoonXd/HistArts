6612aefcd7dde11399a86278d2d80a51f3621aac
PartySoonXd <vladbelom2005@gmail.com>
2024-11-04 11:38:08 +0700

    changed output dir name

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     entry: "./src/index.jsx",
     output: {
-        path: path.resolve(__dirname, "build"),
+        path: path.resolve(__dirname, "dist"),
         publicPath: '/',
         filename: 'bundle.js',
         assetModuleFilename: 'assets/[hash][ext]',
